e8101274608f6780145b50947fe95df428cef851
Josh Graber <josh.graber.85@gmail.com>
2016-02-06 12:08:47 -0500

    When opening a card, ask card list to close cards first. This limits to one card open at a time.

--- components/card-list/card/card.ts
+++ components/card-list/card/card.ts
@@ -1,9 +1,13 @@
-import { Component, Input, TemplateRef } from 'angular2/core';
+import {
+	Component, Input, TemplateRef,
+	Host, Optional, Inject, forwardRef
+} from 'angular2/core';
+
+import { CardList } from '../card-list.ts';
+import { RenderTemplate } from '../../render-template/render-template';
 
 import template from './card.html!text';
 import css from './card.css!text';
-
-import { RenderTemplate } from '../../render-template/render-template';
 
 @Component({
 	selector: 'conf-card',
@@ -24,10 +28,37 @@ export class Card {
 	@Input()
 	card: any;
 
+	private _cardList : CardList;
+
+	constructor(
+		// Make card list optional
+		@Optional() @Host()
+		// Forward reference injection of card list type because card list is defined first
+		@Inject(forwardRef(() => CardList))
+		cardList: CardList) {
+		this._cardList = cardList;
+	}
+
 	showBody: boolean = false;
 
 	// Toggle the display of the body content
 	toggleContent() {
-		this.showBody = !this.showBody;
+		if (this.showBody) {
+			this.close();
+		} else {
+			this.open();
+		}
+	}
+
+	open() {
+		if (this._cardList) {
+			this._cardList.collapseAll();
+		}
+
+		this.showBody = true;
+	}
+
+	close() {
+		this.showBody = false;
 	}
 }
