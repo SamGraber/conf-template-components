5a57cc683423e8b4c18579472288c91cbc1c015b
Josh Graber <josh.graber.85@gmail.com>
2016-02-06 09:31:55 -0500

    Initial commit; basic  angular2 application built on npm and jspm

---
+++ demo/app.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata';
+
+import { bootstrap } from 'angular2/platform/browser';
+
+import { Main } from './main/main';
+
+bootstrap(Main);
---
+++ demo/main/main.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+import template from './main.html!text';
+
+@Component({
+	selector: 'demo-app',
+	template: template,
+})
+export class Main {
+
+}
