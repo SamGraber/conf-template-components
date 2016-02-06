0cc17d02f854888b63345a6566d8996e5912aa5d
Josh Graber <josh.graber.85@gmail.com>
2016-02-06 10:44:15 -0500

    Update main demo app to display a card list

--- demo/main/main.ts
+++ demo/main/main.ts
@@ -1,11 +1,63 @@
 import { Component } from 'angular2/core';
 
+import { CardList, CardHeader, CardBody } from '../../components/card-list/card-list';
+
 import template from './main.html!text';
 
 @Component({
 	selector: 'demo-app',
 	template: template,
+	directives: [CardList, CardHeader, CardBody],
+	styles: [
+		`
+span.important {
+	color: darkred;
+}
+		`,
+	],
 })
 export class Main {
+	sampleData: ITestData[];
 
+	constructor() {
+		this.sampleData = [
+			{
+				dangerous: false,
+				name: 'My first item',
+				value1: 10,
+				value2: 20,
+			},
+			{
+				dangerous: false,
+				name: 'Another item',
+				value1: 12,
+				value2: 6,
+			},
+			{
+				dangerous: true,
+				name: 'Blowing up!',
+				value1: 500,
+				value2: 750,
+			},
+			{
+				dangerous: true,
+				name: 'Big problem',
+				value1: 1000,
+				value2: 900,
+			},
+			{
+				dangerous: false,
+				name: 'Final item',
+				value1: 202,
+				value2: 78,
+			},
+		];
+	}
+}
+
+interface ITestData {
+	dangerous: boolean,
+	name: string,
+	value1: number,
+	value2: number,
 }
